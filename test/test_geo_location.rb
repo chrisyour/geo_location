a3a4b908a6bb3723e6a811a83394911242165a44
Chris Your <chris@ignitionindustries.com>
2011-01-23 19:16:47 -0700

    Version 0.4.2

--- test/test_geo_location.rb
+++ test/test_geo_location.rb
@@ -144,22 +144,4 @@ class TestGeoLocation < Test::Unit::TestCase
     end
     
   end
-  
-  context 'on localhost' do
-    
-    setup do
-      #GeoLocation::use = :hostip
-      GeoLocation::use = :maxmind
-      GeoLocation::key = 'sNHKg3eUWYoT'
-      GeoLocation::dev = nil
-      GeoLocation::dev_ip = nil
-      @location = GeoLocation.find('127.0.0.1')
-    end
-    
-    should "not work properly" do
-      puts @location.inspect
-      assert_equal "nothing", @location[:city]
-    end
-  end
-  
 end
